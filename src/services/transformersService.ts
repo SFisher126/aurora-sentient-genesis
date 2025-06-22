bd068f1045dda4b5b1c1089da64fda240e45da94
SFisher126 <liigogo126@gmail.com>
2025-06-22 16:04:27 -0400

    Fix Transformers quantized model loading error

--- src/services/transformersService.ts
+++ src/services/transformersService.ts
@@ -19,8 +19,8 @@ class TransformersService {
       // Инициализируем анализатор тональности
       this.sentimentAnalyzer = await pipeline('sentiment-analysis', 'Xenova/distilbert-base-uncased-finetuned-sst-2-english');
       
-      // Инициализируем генератор текста с отключенной квантизацией
-      this.textGenerator = await pipeline('text-generation', 'Xenova/gpt2', { quantized: false });
+      // Инициализируем генератор текста без указания quantized опции
+      this.textGenerator = await pipeline('text-generation', 'Xenova/gpt2');
       
       // Инициализируем классификатор текста
       this.textClassifier = await pipeline('text-classification', 'Xenova/distilbert-base-uncased-finetuned-sst-2-english');
